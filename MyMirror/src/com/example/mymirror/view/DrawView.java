cc9f518f0c6da6d38f6eafe2325b7b43084d5952
zjh <Administrator@PC-201701142052>
2018-06-07 17:24:10 +0800

    p123结束

--- MyMirror/src/com/example/mymirror/view/DrawView.java
+++ MyMirror/src/com/example/mymirror/view/DrawView.java
@@ -1,5 +1,22 @@
 package com.example.mymirror.view;
 
-public class DrawView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DrawView extends View{
+	
+	public DrawView(Context context, AttributeSet attrs, int defStyleAttr){
+		super(context,attrs,defStyleAttr);
+	}
+	
+	public DrawView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+	
+	public DrawView(Context context) {
+		this(context, null);
+		//dgasd
+	}
 
 }
