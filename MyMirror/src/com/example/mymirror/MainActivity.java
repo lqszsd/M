cfb3ce4bd70e7c3dc40fcfa0e76fa0007926650f
lqszsd <1658616397@qq.com>
2018-06-07 16:07:59 +0800

    qwe

--- MyMirror/src/com/example/mymirror/MainActivity.java
+++ MyMirror/src/com/example/mymirror/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //6378126e
+        //qweqwe
         
     }
 }
